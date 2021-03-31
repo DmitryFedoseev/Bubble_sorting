c9ee88bc4f3769f2d0fd51018b59a941c465afd6
Dmitry <feddiman02@gmail.com>
2021-03-31 13:56:37 +0300

    creat project

---
+++ SortirovkaPuzyrkom.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int n = 10;
+	int arr[n], f;
+	std::cout << "Enter number to fill the array" << endl;
+
+	for (int i = 0; i < n; i++)									 //Заполнение массива
+	{
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n - 1; i++)								 //Сортировка пузырьком
+	{
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				f = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = f;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)									 //Вывод массива
+		cout << arr[i] << " ";
+
+	cout << endl;
+	
+	return 0;
+}
